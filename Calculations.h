418e5bc831497a18eb4acd546aeebdad4fcebff3
Ethantl28 <75028112+Ethantl28@users.noreply.github.com>
2022-01-24 15:52:50 +0000

    Create Calculations.h

---
+++ Calculations.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class Calculations
+{
+public:
+	int Add(int x, int y)
+	{
+		return x + y;
+	}
+	int Subtract(int x, int y)
+	{
+		return x - y;
+	}
+	int Multiply(int x, int y)
+	{
+		return x * y;
+	}
+	int Divide(int x, int y)
+	{
+		return x / y;
+	}
+};
